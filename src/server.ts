ed45cf15521e5ac81af934e87398f18baaf189a1
tommy8081 <1247527652@qq.com>
2021-10-31 15:24:25 +0800

    db

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,20 @@
 import express, { Express, Request, Response, NextFunction } from 'express';
 import { ApiRouter } from './routes/api-routes';
 import { json, urlencoded } from "body-parser";
-
+import mysql from 'mysql';
 const port = 5000;
 
 const app: Express = express();
 const apiRouter = new ApiRouter;
 const jsonParser = json();
 const urlencodedParser = urlencoded({ extended: false, limit: 1024 * 1024 * 5 });
-
+ // 连接数据库
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "123456",
+    database: "PACS"
+});
 app.use((request: Request, response: Response, next: NextFunction) => {
   response.setHeader("Access-Control-Allow-Origin", "*");
   response.setHeader(
@@ -29,7 +35,11 @@ app.use((request: Request, response: Response) => {
   response.status(404)
   response.send('Page is not found.');
 })
- 
+
+db.connect(err => {
+  if (err) throw err;
+  console.log("数据库连接成功");
+});
 app.listen(port, () => {
   console.log(`server is running on http://localhost:5000`)}
 );
